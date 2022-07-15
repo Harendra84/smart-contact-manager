298a07bf0d6f038c0f302c3190134cd1fce13464
haren <190101120080@cutm.ac.in>
2022-07-15 23:25:34 +0530

    smart-contact

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Smart Contact Manager
+
+#This is full stack web application which uses to add person contact information. 
+Tech: Spring Boot, Security, Thymleaf, HTML, CSS, JAVASCRIPT
+
+
+## Authors
+
+- [@Harendra84](https://www.github.com/octokaterine)
+
+
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+
+## Demo
+
+Insert gif or link to demo
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,50 @@
+console.log("this is script file");
+
+const toggleSidebar =()=>{
+	if($(".sidebar").is(":visible")){
+		$(".sidebar").css("display","none");
+		$(".content").css("margin-left","0%");
+	}
+	else
+	{
+		$(".sidebar").css("display","block");
+		$(".content").css("margin-left","20%");
+	}
+};
+
+const search = () => {
+	let query  = $("#search-input").val();
+
+	if(query == "")
+	{
+		$(".search-result").hide();
+	}
+	else{
+
+		let url  = `http://localhost:8383/search/${query}`;
+
+		fetch(url)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			
+
+			let text = `<div class='list-group'>`;
+
+
+			data.forEach(contact => {
+				text += `<a href='/user/${contact.cId}/contact' class='list-group-item list-group-item-action'> ${contact.name} </a>`
+			});
+
+			
+			text+=`</div>`;
+
+			$(".search-result").html(text);
+			$(".search-result").show();
+		})
+
+
+		$(".search-result").show();
+	}
+};
